f9b9e980a0413ce2343700aaca090fb693ee0fbc
Nalendra Praja <152157640+Cobalttt2311@users.noreply.github.com>
2025-06-03 19:19:39 +0700

    nalendra

--- routes/api.php
+++ routes/api.php
@@ -89,7 +89,7 @@ Route::prefix('users')->group(function () {
 
     Route::put('{userId}/groups/{groupId}', [UserController::class, 'updateGroup']);
 
-    Route::get('{userId}/groups/{groupId}/projects/{projectId}/tasks'. [UserController::class, 'getTaskByProject']);
+    Route::get('{userId}/groups/{groupId}/projects/{projectId}/tasks', [UserController::class, 'getTaskByProject']);
 });
 
 Route::post('/upload-file', [UserController::class, 'upload']);
