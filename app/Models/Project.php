f93554ad0c149c50366330e3b644bd43e9c544ac
Muammar Syahid Rahmallah <syahidrahmallah@gmail.com>
2025-06-14 16:32:52 +0700

    ubah model project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,7 +1,5 @@
 <?php
 
-// app/Models/Project.php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -15,22 +13,23 @@ class Project extends Model
 
     protected $fillable = ['name', 'description', 'startDate', 'endDate', 'groupId', 'userId'];
 
-    // Relasi ke Group (Many to One)
     public function group(): BelongsTo
     {
         return $this->belongsTo(Group::class, 'groupId');
     }
 
-    // Relasi ke Task (One to Many)
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+
     public function tasks(): HasMany
     {
         return $this->hasMany(Task::class, 'projectId');
     }
 
-    // Opsional: Jika kamu ingin mengatur format tanggal secara otomatis
     protected $casts = [
         'startDate' => 'datetime',
         'endDate' => 'datetime',
     ];
 }
-
