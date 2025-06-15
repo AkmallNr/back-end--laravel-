05aaddb23d0f804371daa248274dfd57ca6daa5d
Muammar Syahid Rahmallah <syahidrahmallah@gmail.com>
2025-06-15 18:43:06 +0700

    nambahin controller admin

--- config/auth.php
+++ config/auth.php
@@ -1,38 +1,21 @@
 return [
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'users',
+        'passwords' => 'admins',
     ],
-
     'guards' => [
         'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-        'admin' => [
             'driver' => 'session',
             'provider' => 'admins',
         ],
     ],
-
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
-        ],
         'admins' => [
             'driver' => 'eloquent',
-            'model' => App\Models\Admin::class,
+            'model' => \App\Models\Admin::class,
         ],
     ],
-
     'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => 'password_resets',
-            'expire' => 60,
-            'throttle' => 60,
-        ],
         'admins' => [
             'provider' => 'admins',
             'table' => 'password_resets',
